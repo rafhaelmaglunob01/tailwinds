e3c6821e1040f257bf2014178a1fc02e841d3eca
Matthew Cania <matthewgab24@gmail.com>
2025-03-16 18:49:44 +0800

    namo rafffff

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1
+      className='flex justify-center items-center text-blue-600 font-bold h-screen w-screen'
+    >
+      Test
+    </h1>
+
+  )
+
+}
+
+export default App
